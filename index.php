ab982538904ec428fde6561acaa97126fc94bfd9
Steve Strutt <steve@sjstrutt.com>
2015-05-20 16:33:09 +0000

    cleaned up dn twitter, added lobsters twitter

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ class feed {
             $cache_file = dirname(__FILE__).'/cache/'.$name.'.json';
             $modified = filemtime($cache_file);
             $now = time();
-            $interval = 300; // ten minutes
+            $interval = 10; // five minutes
 
             if ( !$modified || ( ( $now - $modified ) > $interval ) ) {
 
@@ -69,25 +69,53 @@ class feed {
                     fclose( $cache_static );
                 }
 
-                if($matches['feed'] == 'dn-front') {
+            }
 
-                    $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, USER_KEY, USER_SECRET);
-                    $feed_data = json_decode($output);
-                    foreach($feed_data->channel->item as $item) {
+        }
 
-                        if(!filter_var(trim($item->description), FILTER_VALIDATE_URL) === false) {
-                            $connection->post('statuses/update',  array('status' => $item->title.' - '.trim($item->description).' (Discuss @ '.$item->link.')'));
-                        } else {
-                            $connection->post('statuses/update',  array('status' => $item->title.' (Discuss @ '.$item->link.')'));
-                        }
-    
+
+        if($matches['feed'] == 'dn-front') {
+
+            $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, USER_KEY, USER_SECRET);
+            $output = file_get_contents(dirname(__FILE__).'/cache/'.$matches['feed'].'.json');
+            $feed_data = json_decode($output);
+            $counter = 0;
+            foreach($feed_data->channel->item as $item) {
+
+                if($counter < 25) {
+
+                    if(!filter_var(trim($item->description), FILTER_VALIDATE_URL) === false) {
+                        $result = $connection->post('statuses/update',  array('status' => $item->title.' - '.trim($item->description).' (Discuss @ '.str_replace('click/','',$item->link).')'));
+                    } else {
+                        $result = $connection->post('statuses/update',  array('status' => $item->title.' (Discuss @ '.str_replace('click/','',$item->link).')'));
                     }
 
+
                 }
+                $counter++;
 
             }
 
         }
+
+
+        if($matches['feed'] == 'lobsters') {
+
+            $connection = new TwitterOAuth(LOBSTERS_CONSUMER_KEY, LOBSTERS_CONSUMER_SECRET, LOBSTERS_USER_KEY, LOBSTERS_USER_SECRET);
+            $output = file_get_contents(dirname(__FILE__).'/cache/'.$matches['feed'].'.json');
+            $feed_data = json_decode($output);
+            $counter = 0;
+            foreach($feed_data->channel->item as $item) {
+
+                if($counter < 25) {
+                    $result = $connection->post('statuses/update',  array('status' => $item->title.' - '.$item->link.' (Discuss @ '.$item->comments.')'));
+                }
+                $counter++;
+
+            }
+
+        }
+
 
         if(array_key_exists($matches['feed'],$feeds)) {
 
