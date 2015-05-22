59754efee5dc2fb771672a2a635ec49d352e2fee
Steve Strutt <steve@sjstrutt.com>
2015-05-22 14:21:35 +0000

    removed twitter bots--silly, unnecessary and out of scope.

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ ini_set('display_errors', '1');
 
 require_once('lib/glue.php');
 require_once('lib/Savant3-3.0.1/Savant3.php');
-require_once('lib/twitteroauth/autoload.php');
-use Abraham\TwitterOAuth\TwitterOAuth;
 require_once('secrets.php');
 
 $urls = array(
@@ -44,6 +42,8 @@ class feed {
             'dribbble'  => 'https://dribbble.com/shots/popular.rss'
         );
 
+        $updated = array();
+
         foreach($feeds as $name => $url) {
 
             $cache_file = dirname(__FILE__).'/cache/'.$name.'.json';
@@ -52,6 +52,8 @@ class feed {
             $interval = 300; // five minutes
 
             if ( !$modified || ( ( $now - $modified ) > $interval ) ) {
+
+                $updated[] = $name;  
 
                 $context = stream_context_create(array(
                     'http' => array(
@@ -72,50 +74,6 @@ class feed {
             }
 
         }
-
-
-        if($matches['feed'] == 'dn-front') {
-
-            $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, USER_KEY, USER_SECRET);
-            $output = file_get_contents(dirname(__FILE__).'/cache/'.$matches['feed'].'.json');
-            $feed_data = json_decode($output);
-            $counter = 0;
-            foreach($feed_data->channel->item as $item) {
-
-                if($counter < 25) {
-
-                    if(!filter_var(trim($item->description), FILTER_VALIDATE_URL) === false) {
-                        $result = $connection->post('statuses/update',  array('status' => $item->title.' - '.trim($item->description).' (Discuss @ '.str_replace('click/','',$item->link).')'));
-                    } else {
-                        $result = $connection->post('statuses/update',  array('status' => $item->title.' (Discuss @ '.str_replace('click/','',$item->link).')'));
-                    }
-
-
-                }
-                $counter++;
-
-            }
-
-        }
-
-
-        if($matches['feed'] == 'lobsters') {
-
-            $connection = new TwitterOAuth(LOBSTERS_CONSUMER_KEY, LOBSTERS_CONSUMER_SECRET, LOBSTERS_USER_KEY, LOBSTERS_USER_SECRET);
-            $output = file_get_contents(dirname(__FILE__).'/cache/'.$matches['feed'].'.json');
-            $feed_data = json_decode($output);
-            $counter = 0;
-            foreach($feed_data->channel->item as $item) {
-
-                if($counter < 25) {
-                    $result = $connection->post('statuses/update',  array('status' => $item->title.' - '.$item->link.' (Discuss @ '.$item->comments.')'));
-                }
-                $counter++;
-
-            }
-
-        }
-
 
         if(array_key_exists($matches['feed'],$feeds)) {
 
